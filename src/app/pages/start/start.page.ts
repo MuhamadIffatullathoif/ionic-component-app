657c7c8905cb64ed876f39e80a597452cab32295
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-02-09 13:09:28 +0700

    05. ion-list List in ionice

--- src/app/pages/start/start.page.ts
+++ src/app/pages/start/start.page.ts
@@ -1,12 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+interface ComponentInterface {
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
 @Component({
   selector: 'app-start',
   templateUrl: './start.page.html',
   styleUrls: ['./start.page.scss'],
 })
 export class StartPage implements OnInit {
-
+  public components: ComponentInterface[] = [
+    {
+      icon: 'american-football-outline',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'logo-apple-appstore',
+      name: 'Alert',
+      redirectTo: '/alert'
+    },
+  ]
   constructor() { }
 
   ngOnInit() {
