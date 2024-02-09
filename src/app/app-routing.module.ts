35a9b4458a13bc9f213691ad859e09fa341fca7c
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-02-09 08:28:12 +0700

    02. Navigation Between Pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,21 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'start',
+    pathMatch: 'full'
+  },
+  {
     path: 'start',
     loadChildren: () => import('./pages/start/start.module').then(m => m.StartPageModule)
   },
   {
-    path: '',
-    redirectTo: 'start',
-    pathMatch: 'full'
+    path: 'alert',
+    loadChildren: () => import('./pages/alert/alert.module').then( m => m.AlertPageModule)
+  },
+  {
+    path: 'action-sheet',
+    loadChildren: () => import('./pages/action-sheet/action-sheet.module').then( m => m.ActionSheetPageModule)
   },
 ];
 
